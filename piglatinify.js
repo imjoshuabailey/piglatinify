95adff9a5e01cf6949c27fd1d339ce8a08ebb492
Josh Bailey <joshbailey@Annes-MacBook-Pro.local>
2018-03-21 19:05:32 -0700

    initial commit

---
+++ piglatinify.js
@@ -0,0 +1,23 @@
+function piglatin(english) {
+	var words = english.split(" ");
+	console.log(words);
+	var vowels = "a, e, i, o, u";
+	var changedWords = [];
+
+	var result = words.map(function(word) {
+		for (a = 0; a < word.length; a++) {
+			// console.log("index:", a);
+			for (b = 0; b < vowels.length; b++) {
+				if(word[a] == vowels[b]) {
+					var vowel = word.slice(a);
+					var cons = word.slice(0, a);
+					return changedWords.push(vowel + "-" + cons + "ay");
+				}
+			}
+		}
+	});
+	console.log(changedWords);
+}
+
+
+piglatin("change these words!")
