5211a103b8f0953eb02a28ec18a3fa69181a5017
Josh Bailey <joshbailey@Annes-MacBook-Pro.local>
2018-04-03 23:11:19 -0700

    remove 'u' from vowels if 'q' comes before the first vowel, if word starts with a vowel add '-way' to the end

--- piglatinify.js
+++ piglatinify.js
@@ -1,24 +1,54 @@
 
+// function takes in a sentence and returns the sentence in pig latin
 function piglatin(english) {
-	const vowels = ["a", "e", "i", "o", "u"];
-
-	var words = english.split(" ");
-	var changedWords = [];
-
-	var result = words.map(function(word) {
-		for (a = 0; a < word.length; a++) {
-			for (b = 0; b < vowels.length; b++) {
-				if(word[a] == vowels[b]) {
-					var vowel = word.slice(a);
-					var cons = word.slice(0, a);
-					return changedWords.push(vowel + "-" + cons + "ay");
-				}
-			}
-		}
+	const results = english.split(" ").map(function(word) { // map over the words
+		return pigifyWord(word)
 	});
 
-	return changedWords.join(" ")
+	return results.join(" ");
+}
+
+function pigifyWord(word) {
+	const vowels = ["a", "e", "i", "o", "u"];
+
+	for (a = 0; a < word.length; a++) {
+		if(word[a] == "q") {
+			 vowels.pop();
+		 }
+		for (b = 0; b < vowels.length; b++) {
+			if(word[0] == vowels[b]){
+				return word + "-way"
+			} else if(word[a] == vowels[b]) {
+				var vowel = word.slice(a);
+				var cons = word.slice(0, a);
+				var word = vowel + "-" + cons + "ay"
+
+				return movePunctuationToEnd(word);
+			}
+
+		}
+	}
+}
+
+function movePunctuationToEnd(word) {
+	const punctuation = [".", "!", "?"]
+
+	var toEnd = [];
+
+	for (var i = 0; i < punctuation.length; i++) {
+
+		for (var index = word.indexOf(punctuation[i]); index > -1; index = word.indexOf(punctuation[i])) {
+			toEnd.push(word[index])
+
+			word = word.slice(0, index) + word.slice(index + 1)
+		}
+	}
+
+	return word + toEnd.join('')
 }
 
 
-console.log(piglatin("change these words!"));
+
+
+
+console.log(piglatin("The quick brown. fox jumped over the lazy dog!"));
